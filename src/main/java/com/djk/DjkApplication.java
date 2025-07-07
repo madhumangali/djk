0452ea2410c83e5861619a3ee79419ba4f3196d7
Madhu <Mangali.Madhu@skillassure.com>
2025-07-07 18:59:55 +0530

    first commit

---
+++ src/main/java/com/djk/DjkApplication.java
@@ -0,0 +1,13 @@
+package com.djk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djk/DjkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
